44cccecab912379d099b090832b535cdf1865bda
ahorupa <38441297+ahorupa@users.noreply.github.com>
2021-12-16 07:42:59 +0100

    initial commit

---
+++ documents/forms.py
@@ -0,0 +1,8 @@
+# from django import forms
+# from.models import Comment
+
+
+# class Comments(forms.ModelForm):
+#     class Meta:
+#         fields = '__all__'
+#         model = Comment
---
+++ useraccount/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from.views import*
+
+urlpatterns = [
+    path('', hi, name="hello"),
+    # path('api/register/', RegisterViews.as_view(), name="register"),
+]
